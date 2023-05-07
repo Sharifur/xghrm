2120ecea73ec995c450f9b185cc40e0a5c8bffc1
Sharifur <sharifurrahman@Sharifurs-MacBook-Pro.local>
2023-05-08 00:58:17 +0600

    style chnage

--- public/js/main-script.js
+++ public/js/main-script.js
@@ -1,1 +1,11 @@
-document.addEventListener("DOMContentLoaded",(function(){console.log("dom content ready")}));
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!*************************************!*\
+  !*** ./resources/js/main-script.js ***!
+  \*************************************/
+document.addEventListener("DOMContentLoaded", function () {
+  /* run code after dome content loaded */
+  console.log('dom content ready');
+});
+/******/ })()
+;
