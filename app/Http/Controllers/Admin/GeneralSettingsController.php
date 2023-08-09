257e74ed53b4301ffc718c713fc7520b4185dfd7
Sharifur Rahman <rsharifur824@gmail.com>
2023-08-09 18:37:19 +0600

    remote work count issue solved

--- app/Http/Controllers/Admin/GeneralSettingsController.php
+++ app/Http/Controllers/Admin/GeneralSettingsController.php
@@ -40,7 +40,7 @@ class GeneralSettingsController extends Controller
         $getUsers =  $zkHelper->users();
         $allEmployee = Employee::where('status',1)->get();
         $buildDataForAttendanceLog = [];
-
+dd($getData);
         $filename = 'attendance-file/last-30days-attendance'.Str::uuid()->toString().'.csv';
         file_put_contents($filename,'');
         $fl =  fopen(public_path($filename),'w');
