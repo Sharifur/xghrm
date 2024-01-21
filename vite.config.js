c635f16ebdc62bb6781e923655edc47447e31608
Sharifur Rahman <rsharifur824@gmail.com>
2024-01-21 13:27:37 +0600

    app.css not working on server

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,10 @@ import path from 'path'; // Import the path module
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.js',
+            input:  [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
         vue(
